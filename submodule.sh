3ba7bfaf7aac9265f65f838104086c6c881e3715
xinmingyao <yaoxinming@gmail.com>
2014-06-27 17:59:28 +0800

    modify make

---
+++ submodule.sh
@@ -0,0 +1,6 @@
+ git submodule add https://github.com/xinmingyao/lbase64 3rd/base64
+ git submodule add https://github.com/xinmingyao/luasec.git 3rd/luasec
+ git submodule add https://github.com/mkottman/luacrypto 3rd/luacrypto
+ git submodule add https://github.com/cisco/libsrtp 3rd/libsrtp
+ git submodule add https://github.com/xinmingyao/lua_srtp 3rd/lua_srtp
+ git submodule add https://github.com/lua/lpeg 3rd/lpeg
