006fa415050e450c74a5be32955a70072eb64f93
xinmingyao <yaoxinming@gmail.com>
2014-07-24 16:23:11 +0800

    add udp support

--- skynet-src/skynet_socket.h
+++ skynet-src/skynet_socket.h
@@ -13,6 +13,8 @@ struct skynet_socket_message {
 	int type;
 	int id;
 	int ud;
+  char peer_ip[20]; //for udp
+  int  peer_port; //for udp
 	char * buffer;
 };
 
@@ -22,9 +24,11 @@ void skynet_socket_free();
 int skynet_socket_poll();
 
 int skynet_socket_send(struct skynet_context *ctx, int id, void *buffer, int sz);
+int skynet_socket_send_udp(struct skynet_context *ctx, int id, void *buffer, int sz,char *peer_ip,int peer_port);
 void skynet_socket_send_lowpriority(struct skynet_context *ctx, int id, void *buffer, int sz);
 int skynet_socket_listen(struct skynet_context *ctx, const char *host, int port, int backlog);
 int skynet_socket_connect(struct skynet_context *ctx, const char *host, int port);
+int skynet_socket_connect_udp(struct skynet_context *ctx, const char *host, int port);
 int skynet_socket_bind(struct skynet_context *ctx, int fd);
 void skynet_socket_close(struct skynet_context *ctx, int id);
 void skynet_socket_start(struct skynet_context *ctx, int id);
